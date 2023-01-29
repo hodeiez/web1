e6db1073813aa9459d696341c5c859dd5ee1cac7
hodeiez <hodeinaiz@hotmail.com>
2023-01-29 23:47:04 +0100

    check response

--- web1/src/views/AboutView.vue
+++ web1/src/views/AboutView.vue
@@ -11,7 +11,8 @@ import { callWithAsyncErrorHandling, onMounted, onUnmounted, ref, watchEffect } 
 // const test=import.meta.env.VITE_TEST
 //test fetch
 // fetch('/api/infocards/2020/2023').then(r=>r.json()).then(d=>getTest.value=d)
-fetch('/api/infocards/2020/2023').then(r=>console.log(r.json())).then(d=>console.log(JSON.stringify(d)))
+fetch('/api/infocards/2020/2023').then(r=>console.log(r))
+fetch('http://hodei-web1.onrender.com/api/infocards/2020/2023').then(r=>console.log(r))
 // const fetchTest=async()=>{
 
 //     const res=await fetch('/api/infocards/2020/2023')
